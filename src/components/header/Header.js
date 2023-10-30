07ff370ad78f9f0396151c59917497cb5c98c0bf
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-10-30 21:03:14 +0200

    feat:add palestine-flag

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import './header/style.css'
-
 import FullWidthGrid from './header/HeaderMain';
-
+// import PALESTINE from 'src/assets/palestine-flag.png'
 export default function Header() {
 
   return (
     <header style={{position:"static",paddingTop:"2.5rem"}}>
-      <nav className='navbar'>
+    
+
+      <nav className='navbar'>      <div className='block pr-10' >
+      <img src={'https://flagemoji.net/img/flags/palestine-flag.png'} alt={"palestine-flag"} width={"50px"}  height={"auto"} style={{padding:'0px 10px'}} loading="lazy" />
+
+
+STAND WITH PALESTINE
+<img src={'https://flagemoji.net/img/flags/palestine-flag.png'} alt={"palestine-flag"} width={"50px"}  height={"auto"} style={{padding:'0px 10px'}} loading="lazy" />
+
+
+      </div>
         <div className='navbar-content'>
          <a href='tel:+201550003860'> +20 155 000 3860</a>
         </div>

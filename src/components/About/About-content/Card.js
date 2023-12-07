03e8a84cb48eb3665172f9e1dab8533738c11e4a
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-12-07 17:24:39 +0200

    update:code

--- src/components/About/About-content/Card.js
+++ src/components/About/About-content/Card.js
@@ -1,34 +1,24 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
 
-import CardContent from '@mui/material/CardContent';
+import CardContent from "@mui/material/CardContent";
 
+import Avatar from "@mui/material/Avatar";
 
-import Avatar from '@mui/material/Avatar';
-
-export default function BasicCard({card}) {
-
+export default function BasicCard({ card }) {
   return (
-    <Card sx={{ maxWidth: '80%' }} className='center'>
+    <Card sx={{ maxWidth: "80%" }} className="center">
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: '#ff9301' }} aria-label="recipe">
+          <Avatar sx={{ bgcolor: "#ff9301" }} aria-label="recipe">
             {card.icon}
-           
           </Avatar>
         }
-
-
       />
       <CardHeader title={card.name} />
-      <CardContent>
-        {card.text}
-      
-
-      </CardContent>
-
+      <CardContent>{card.text}</CardContent>
     </Card>
   );
 }
